485ad5903f159af99a36045e7b9a2607ff858707
maximiloi <maximiloi@gmail.com>
2026-06-16 14:16:46 +0300

    feat: add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['10.0.1.9'],
 };
 
 export default nextConfig;
