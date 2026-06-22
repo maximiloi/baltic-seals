924e38fc1de4932b2f0a495ede79ee71be180b99
maximiloi <maximiloi@gmail.com>
2026-06-22 11:46:21 +0300

    feat: add pm2 config file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'baltic-seals',
+      script: 'npm',
+      args: 'start',
+      cwd: '/var/www/baltic-seals.ru',
+      env: {
+        PORT: 3002,
+        GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
+        GOOGLE_SHEET_API_KEY: process.env.GOOGLE_SHEET_API_KEY,
+        NODE_ENV: 'production',
+      },
+      error_file: '/var/www/baltic-seals.ru/logs/err.log',
+      out_file: '/var/www/baltic-seals.ru/logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
+};
