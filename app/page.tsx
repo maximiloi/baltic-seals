aa8938ff2caa3c359d346f57be1e21d4ad0472e3
maximiloi <maximiloi@gmail.com>
2026-07-02 10:56:41 +0300

    feat: add new section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FlagFootballSection } from '@/components/FlagFootballSection';
 import { StatsSection } from '@/components/StatsSection';
 import { TournamentsSection } from '@/components/TournamentsSection';
 import { PracticesSectionContentClient } from '@/components/PracticesSection';
+import { HowToJoinSection } from '@/components/HowToJoinSection';
 import { SponsorsSection } from '@/components/SponsorsSection';
 import { Footer } from '@/components/Footer';
 import { getTournaments } from '@/lib/tournaments';
@@ -27,6 +28,7 @@ export default async function Home() {
       <StatsSection tournaments={tournaments} />
       <TournamentsSection tournaments={tournaments} />
       <PracticesSectionContentClient practices={practices} />
+      <HowToJoinSection />
       <SponsorsSection />
       <Footer />
     </main>
